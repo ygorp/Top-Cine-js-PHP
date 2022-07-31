885f80d729bd2361ce94a2d931bfc3e00065ddc4
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2022-07-31 20:11:36 -0300

    criando um site de filmes, foi criada a página inicial com menus, card com capa descrição e nota dos filmes. agora criando página de cadastro dos filmes.

---
+++ cadastrar.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <title>Cadastrar</title>
+</head>
+
+<body>
+  <nav class="nav-extended  indigo lighten-1">
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="galeria.php">Galeria</a></li>
+        <li><a href="cadastrar.php">Cadastrar</a></li>
+      </ul>
+    </div>
+
+    <div class="nav-header">
+      <h1>TopCine</h1>
+    </div>
+
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent indigo darken-4">
+        <li class="tab"><a class="active" href="#test1">Todos</a></li>
+        <li class="tab"><a href="#test2">Assistidos</a></li>
+        <li class="tab"><a href="#test3">Favoritos</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="row">
+    <div class="col s6 offset-s3">
+      <div class="card">
+        <div class="card-content">
+          <span class="card-title">Cadastrar Filme</span>
+
+          <div class="row">
+            <div class="input-field col s12">
+              <label for="titulo">Titulo do filme</label>
+              <input id="titulo" type="text" class="validate">
+            </div>
+          </div>
+
+          <div class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s12">
+                  <textarea id="sinopse" class="materialize-textarea"></textarea>
+                  <label for="sinopse">Sinopse</label>
+                </div>
+              </div>
+            </form>
+          </div>
+
+        </div>
+        <div class="card-action">
+          <a class="btn waves-effect waves-light indigo lighten-1" href="galeria.php">Cancelar</a>
+          <a href="#" class="waves-effect waves-light btn indigo darken-4">Salvar</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ galeria.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+  <title>TopCine</title>
+</head>
+
+<body>
+  <nav class="nav-extended  indigo lighten-1">
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="galeria.php">Galeria</a></li>
+        <li><a href="cadastrar.php">Cadastrar</a></li>
+      </ul>
+    </div>
+
+    <div class="nav-header">
+      <h1>TopCine</h1>
+    </div>
+
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent indigo darken-4">
+        <li class="tab"><a class="active" href="#test1">Todos</a></li>
+        <li class="tab"><a href="#test2">Assistidos</a></li>
+        <li class="tab"><a href="#test3">Favoritos</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="row">
+    <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/w300/u49fzmIJHkb1H4oGFTXtBGgaUS1.jpg">
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+          <p class="valign-wrapper"><i class="material-icons amber-text">star</i> 9.8</p>
+          <span class="card-title">Vingadores</span>
+          <p>Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/w300/pkRLUQQdlve7H7LjhyNODF7F5mK.jpg">
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+          <p class="valign-wrapper"><i class="material-icons amber-text">star</i> 9.8</p>
+          <span class="card-title">John Wick - Um novo dia para matar</span>
+          <p>O lendário John Wick é forçado a deixar a aposentadoria em função de um criminoso que conspira para tomar o controle de um clã de assassinos internacionais. Por causa de um pacto de sangue, John Wick viaja para Roma, com o objetivo de ajudar um velho amigo a derrubar a organização internacional secreta, perigosa e mortal de assassinos procurados em todo o mundo.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/w300/dpd9aUIUT0tUDIard91nTWLLETV.jpg">
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+          <p class="valign-wrapper"><i class="material-icons amber-text">star</i> 9.8</p>
+          <span class="card-title">Código de conduta</span>
+          <p>Quando um dos suspeitos do assassinato de sua mulher e filha é solto, Clyde quer vingança e decide fazer justiça com as próprias mãos. Clyde é preso e dentro da cadeia organiza uma matança para desmascarar o sistema judicial corrupto.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/w300/hq2igFqb31fDqGotz8ZuUfwKgn8.jpg">
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+          <p class="valign-wrapper"><i class="material-icons amber-text">star</i> 9.8</p>
+          <span class="card-title">DR.Estranho - Multiverso da loucura</span>
+          <p>Doutor Estranho, com a ajuda de aliados místicos antigos e novos, atravessa as perigosas realidades alternativas e alucinantes do Multiverso para enfrentar um novo adversário misterioso.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
