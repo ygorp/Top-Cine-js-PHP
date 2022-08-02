b7fd2fe49465028d6fbae483e7216b94989bb105
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2022-08-01 23:11:59 -0300

    criando arquivo php com código do cabeçalho para fazcilitar estrutura e estilo das outras páginas

--- cadastrar.php
+++ cadastrar.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="PT-BR">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <!-- Compiled and minified CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-  <!-- Compiled and minified JavaScript -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <title>Cadastrar</title>
-</head>
+<?php include "cabecalho.php" ?>
 
 <body class="grey lighten-1">
   <nav class="nav-extended  indigo lighten-1">
